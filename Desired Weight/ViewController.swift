8dfce963093a84620b4898e552660f19d8df6710
Long Nguyen <elongmusk39@gmail.com>
2020-07-11 11:36:34 -0700

    Initial Commit

---
+++ Desired Weight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Desired Weight
+//
+//  Created by Long Nguyen on 7/11/20.
+//  Copyright © 2020 Long Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
